ee247534fc709648a62c5d4d531d59d13ca48ec6
Benjamin <127534872+Bengoro1@users.noreply.github.com>
2023-04-27 12:25:08 +0200

    add loop 1st to 5 points wins, alert score

--- game.js
+++ game.js
@@ -1,36 +1,52 @@
-let computerChoice
-let getComputerChoice = () => {
+let computerChoice;
+function getComputerChoice () {
     let randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber === 0) {
-        computerChoice = "Rock";
+        computerChoice = "rock";
     } else if (randomNumber === 1) {
-        computerChoice = "Paper";
+        computerChoice = "paper";
     } else {
-        computerChoice = "Scissors";
+        computerChoice = "scissors";
     }
-    console.log(randomNumber);
-    console.log(computerChoice)
     randomNumber = Math.floor(Math.random() * 3);
 }
 
-let promptChoice = prompt("What is your choice...?")
-let playerSelection = promptChoice.toLowerCase();
-let playRound = () => {
-    if (playerSelection == "rock" && computerChoice === "Paper") {
-        return "Rock beats paper, you win!";
-    } else if (playerSelection == "rock" && computerChoice === "Scissors") {
-        return "Scissors beat rock, you lose!";
-    } else if (playerSelection == "paper" && computerChoice === "Rock") {
-        return "Paper beats rock, you win!";   
-    } else if (playerSelection == "paper" && computerChoice === "Scissors") {
-        return "Scissors beat paper, you lose!";  
-    } else if (playerSelection == "scissors" && computerChoice === "rock") {
-        return "Rock beats scissors, you lose!";   
-    } else if (playerSelection == "scissors" && computerChoice === "paper") {
-        return "scissors beats paper, you win!";  
-    } else {
+let promptChoice;
+let playerSelection;
+let playerScore = 0;
+let computerScore = 0;
+
+function playRound () {    
+    getComputerChoice();
+    promptChoice = prompt("What is your choice...?", "Rock");
+    playerSelection = promptChoice.toLowerCase();   
+    if (playerSelection === computerChoice) {
         return "It's a tie!";
-    } 
+    } else if ((playerSelection == "rock" && computerChoice == "paper") || (playerSelection == "paper" && computerChoice == "scissors") || (playerSelection == "scissors" && computerChoice == "rock")) {
+        computerScore++
+        return "You lose!, " + computerChoice + " beats " + playerSelection;
+    } else if ((playerSelection == "rock" && computerChoice == "scissors") || (playerSelection == "paper" && computerChoice == "rock") || (playerSelection == "scissors" && computerChoice == "paper")) {
+        playerScore++
+        return "You win!, " + playerSelection + " beats " + computerChoice;
+    } else {
+        return "Invalid entry";
+    }    
 }
-getComputerChoice();
-console.log(playRound());
+
+function game() {
+    do {
+        alert(playRound());
+        if (playerScore == 5) {
+            playerScore = 0;
+            computerScore = 0;
+            alert("You won! You have beaten the computer.")
+        } else if (computerScore == 5) {
+            playerScore = 0;
+            computerScore = 0;
+            alert("You lost! The computer beat you.")
+        } else {
+            alert(`Your score is: ${playerScore}. Computer's score is: ${computerScore}.`)
+        }
+    } while ((playerScore < 5) && (computerScore < 5));   
+}
+ game();
